ee357503f3442df60b10bb21fd8a3eb22a446bfd
Rajbhaiya1 <104322683+Rajbhaiya1@users.noreply.github.com>
2023-10-25 22:49:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,5 @@
-cd /app
 rm -rf mangabot
-git clone https://github.com/Dra-Sama/mangabot
+git clone https://github.com/Rajbhaiya1/mangabot
 cd mangabot
 pip install --quiet -r requirements.txt
 python main.py
